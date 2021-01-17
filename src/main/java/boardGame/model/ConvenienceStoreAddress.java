7ba4fac71bb2a734057173fe1e161db84c74ba21
githubhuangkai <74950136+githubhuangkai@users.noreply.github.com>
2021-01-17 16:17:52 +0800

    2021/01/17/16.17

---
+++ src/main/java/boardGame/model/ConvenienceStoreAddress.java
@@ -0,0 +1,69 @@
+package boardGame.model;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//縣市
+@Entity
+public class ConvenienceStoreAddress {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer convenienceStoreAddressId;
+
+	private String convenienceStoreAddress;
+	
+	@ManyToOne
+	@JsonIgnore
+	private ConvenienceStoreType convenienceStoreType;
+
+	@ManyToOne
+	@JsonIgnore
+	private Road road;
+
+	public ConvenienceStoreAddress() {
+		super();
+	}
+
+	public ConvenienceStoreAddress(String convenienceStoreAddress, ConvenienceStoreType convenienceStoreType,
+			Road road) {
+		super();
+		this.convenienceStoreAddress = convenienceStoreAddress;
+		this.convenienceStoreType = convenienceStoreType;
+		this.road = road;
+	}
+
+	public String getConvenienceStoreAddress() {
+		return convenienceStoreAddress;
+	}
+
+	public void setConvenienceStoreAddress(String convenienceStoreAddress) {
+		this.convenienceStoreAddress = convenienceStoreAddress;
+	}
+
+	public Integer getConvenienceStoreAddressId() {
+		return convenienceStoreAddressId;
+	}
+
+	public ConvenienceStoreType getConvenienceStoreType() {
+		return convenienceStoreType;
+	}
+
+	public Road getRoad() {
+		return road;
+	}
+	
+}
